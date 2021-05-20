5afa96736a2f621b605e34481c12245372647e3b
Rhinous <rcottuli00@gmail.com>
2021-05-20 12:09:21 +1000

    Add project files.

---
+++ Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Position
+    {
+        private Date Start;
+        private Date End;
+
+        public string Title()
+        {
+
+        }
+    }
+}
---
+++ Researcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Researcher
+    {
+        private int id;
+        private string GivenName;
+        private string FamilyName;
+        private string Title;
+        private string School;
+        private string Campus;
+        private string Email;
+
+
+        public position GetCurrentJob()
+        {
+
+        }
+
+        public string CurrentJobTitle()
+        {
+
+        }
+
+        public Date CurrentJobStart()
+        {
+
+        }
+
+        public Position GetEarliestJob()
+        {
+
+        }
+
+        public Date EarliestStart()
+        {
+
+        }
+
+        public float Tenure()
+        {
+
+        }
+
+        public int PublicationsCount()
+        {
+
+        }
+    }
+}
